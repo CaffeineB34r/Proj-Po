42ab9ea6e0032f85c05d755cf7fc1167ed083262
Matilde Lopes <matilde.rocha.lopes@gmail.com>
2022-11-03 00:01:21 +0000

    Communication

--- prr/core/Communication.java
+++ prr/core/Communication.java
@@ -1,7 +1,6 @@
 package prr.core;
 
 import java.io.Serializable;
-
 import prr.core.Client.ClientLevel;
 
 abstract public class Communication implements Serializable{
@@ -12,15 +11,89 @@ abstract public class Communication implements Serializable{
     private boolean _isPaid;
     private double _cost;
     private boolean _isOngoing;
+    private Type _type;
+    private Status _status;
+    private Terminal _terminalFrom;
+    private Terminal _terminalTo;
+    private int _units;
+    private double _price;
+    private int _size;
 
-    public Communication(int id, double cost){
+
+    public Communication(Type type, Status status ,int units, int id, double cost, Terminal terminalFrom, Terminal terminalTo, double price){
         _id = id;
         _cost = cost;
+        _type = type;
+        _status = status;
+        _units = units;
+        _terminalFrom = terminalFrom; 
+        _terminalTo = terminalTo;
+        _price = price;
+        _size = 0;
+    }
+
+    public void setDuration(){
+
     }
 
     public int getSize(){
-        
+
     }
+
+    public enum Type{
+        VOICE,
+        TEXT,
+        VIDEO;
+    }
+
+    public enum Status{
+        ONGOING,  //communication in progress 
+        FINISHED; //communication ended
+    }
+
+    public Type getType(){
+        return _type;
+    }
+
+    public Status getStatus(){
+        return _status;
+    }
+
+    public int getIdCom(){
+        return _id;
+    }
+
+    public Terminal getIdSender(){
+        return _terminalFrom;
+    }
+
+    public Terminal getIdReceiver(){
+        return _terminalTo;
+    }
+
+    public int getUnits(){
+        return _units;
+    }
+
+    public double getPrice(){
+        return _price;
+    }
+
+    public String toString(){
+        //type|idCommunication|idSender|idReceiver|units|price|status
+        StringBuilder sb = new StringBuilder("type");
+
+        sb.append(getType()+"|");
+        sb.append(getIdCom()+"|");
+        sb.append(getIdSender()+"|");
+        sb.append(getIdReceiver()+"|");
+        sb.append(getUnits()+ "|");
+        sb.append(getPrice()+"|");
+        sb.append(getStatus()+"|");
+
+        return sb.toString();
+    }
+
 
     protected double computeCost(ClientLevel client){
         
@@ -30,3 +103,4 @@ abstract public class Communication implements Serializable{
 
 
 
+\
